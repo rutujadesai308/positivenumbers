59ac1bb8213b40a0defb83b56ac19b092d024e59
rutujadesai308 <73026172+rutujadesai308@users.noreply.github.com>
2020-10-28 14:14:34 +0530

    created positiveint.py
    this program will display all the positive numbers from the given list

---
+++ positiveint.py
@@ -0,0 +1,11 @@
+list1=[12,-7,5,64,-14]
+for i in list1:
+    if (i<0):
+        continue
+    print(i)
+print("----------------")
+list2=[12,14,-95,3]
+for j in list2:
+    if(j<0):
+        continue
+    print(j)
